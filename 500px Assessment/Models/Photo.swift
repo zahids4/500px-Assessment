e5e3f4f49c892408864c50e61ca7c0b1c683c814
Saim Zahid <saimzahid@me.com>
2019-10-25 17:37:22 -0400

    Creating Models for PopularPhotos and Photo

---
+++ 500px Assessment/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  500px Assessment
+//
+//  Created by Saim Zahid on 2019-10-25.
+//  Copyright © 2019 Saim Zahid. All rights reserved.
+//
+
+import Foundation
+
+public struct Photo: Decodable {
+    public let name: String
+    public let imageUrl: [String]
+}
---
+++ 500px Assessment/Models/PopularPhotos.swift
@@ -0,0 +1,13 @@
+//
+//  PopularPhotos.swift
+//  500px Assessment
+//
+//  Created by Saim Zahid on 2019-10-25.
+//  Copyright © 2019 Saim Zahid. All rights reserved.
+//
+
+import Foundation
+
+public struct PopularPhotos: Decodable {
+    public let photos: [Photo]
+}
