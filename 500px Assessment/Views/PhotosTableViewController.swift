9e1557fb441562e77dd161ade3051577c1200f0c
Saim Zahid <saimzahid@me.com>
2019-10-27 15:36:53 -0400

    Setting image on photo cell

--- 500px Assessment/Views/PhotosTableViewController.swift
+++ 500px Assessment/Views/PhotosTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Alamofire
 
 class PhotosTableViewController: UITableViewController {
+    fileprivate let operations = ImageDownloadOperations()
     fileprivate var photoViewModels = [PhotoViewModel]() {
         didSet {
             tableView.reloadData()
